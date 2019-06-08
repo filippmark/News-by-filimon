a64c2c5c48b2eaf1aed59c898f5a908baecc930e
filippmark <lenovofm12@outlook.com>
2019-06-08 17:14:00 +0300

    Updates

---
+++ precache-manifest.1c7a40391160d40075e721a72a285466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "486b65bcc2ab38c4d6d9",
+    "url": "/news-app/static/css/main.d55c8042.chunk.css"
+  },
+  {
+    "revision": "486b65bcc2ab38c4d6d9",
+    "url": "/news-app/static/js/main.d33769b0.chunk.js"
+  },
+  {
+    "revision": "fa4b01bc436166f464fa",
+    "url": "/news-app/static/js/runtime~main.dc36b5e3.js"
+  },
+  {
+    "revision": "750e6f4c736d89ff6d40",
+    "url": "/news-app/static/js/2.8c7cc350.chunk.js"
+  },
+  {
+    "revision": "ab6aafcdc914511e16adab54ae0c9138",
+    "url": "/news-app/index.html"
+  }
+];
